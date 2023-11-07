c4add5040b49e635a7f67587e361e8f3bf6c8d42
yoosion030 <yoosioff@gmail.com>
2023-11-07 14:38:58 +0900

    Update header style

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,27 +5,39 @@ import styled from "@emotion/styled";
 const Header = () => {
   return (
     <HeaderStyle>
-      <Logo className="box">나이키</Logo>
-      <p>.</p>
+      <Logo className="box" href="/">
+        ASAP
+      </Logo>
+      <Nav>
+        <a href="/nike">Nike</a>
+      </Nav>
     </HeaderStyle>
   );
 };
 
 const HeaderStyle = styled.header`
   width: 100vw;
-  height: 70px;
+  height: 52px;
   display: flex;
   align-items: center;
-  padding: 0 70px;
-  justify-content: space-between;
+  padding: 0 440px;
   position: fixed;
   z-index: 50;
-  background-color: #ffffff;
+  background-color: rgba(255, 255, 255, 0.4);
+  border-bottom: rgba(255, 255, 255, 0.24);
+  gap: 100px;
 `;
 
-const Logo = styled.h2`
-  font-size: 32px;
+const Logo = styled.a`
+  font-size: 16px;
   font-weight: 900;
+`;
+
+const Nav = styled.nav`
+  display: flex;
+  gap: 100px;
+  font-size: 12px;
+  font-weight: 400;
 `;
 
 export default Header;
