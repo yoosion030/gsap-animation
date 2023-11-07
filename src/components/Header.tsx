1fe481704dbcf3d087414e7ebeba6cdb344c73fb
yoosion030 <yoosioff@gmail.com>
2023-11-07 10:19:56 +0900

    Add test header

---
+++ src/components/Header.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import styled from "@emotion/styled";
+
+const Header = () => {
+  return (
+    <HeaderStyle>
+      <Logo className="box">나이키</Logo>
+      <p>.</p>
+    </HeaderStyle>
+  );
+};
+
+const HeaderStyle = styled.header`
+  width: 100vw;
+  height: 70px;
+  display: flex;
+  align-items: center;
+  padding: 0 70px;
+  justify-content: space-between;
+  position: fixed;
+  z-index: 100;
+  background-color: #ffffff;
+`;
+
+const Logo = styled.h2`
+  font-size: 32px;
+  font-weight: 900;
+`;
+
+export default Header;
